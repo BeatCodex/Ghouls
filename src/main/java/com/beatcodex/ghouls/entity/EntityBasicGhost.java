f90dfe370711d414e0ecce998dddb0cb61279be7
unknown <jamadd1212@gmail.com>
2014-06-23 17:16:57 -0400

    Corrected ghost type hierarchy in anticipation of multiple ghost types

---
+++ src/main/java/com/beatcodex/ghouls/entity/EntityBasicGhost.java
@@ -0,0 +1,11 @@
+package com.beatcodex.ghouls.entity;
+
+import net.minecraft.world.World;
+
+public class EntityBasicGhost extends EntityGhost {
+
+	public EntityBasicGhost(World par1World) {
+		super(par1World);
+	}
+
+}
